a426bf8a16b5d79a3227137532048b270165ee79
Kyle Brown <brow1144@purdue.edu>
2017-06-14 18:35:52 -0400

    Title and message field have methods that update the state of the application

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -2,15 +2,58 @@ import React, { Component } from 'react'
 import './NoteForm.css'
 
 class NoteForm extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            notes: {
+                title: '',
+                message: '',
+            },
+        }
+
+        this.onTitleChange = this.onTitleChange.bind(this)
+        this.onMessageChange = this.onMessageChange.bind(this)
+    }
+
+    onTitleChange(ev) {
+        let updatedNotes = this.state.notes
+        updatedNotes.title = ev.target.value
+        
+        this.setState({
+            notes: updatedNotes
+        })
+    }
+
+    onMessageChange(ev) {
+        let updatedNotes = this.state.notes
+        updatedNotes.message = ev.target.value
+        
+        this.setState({
+            notes: updatedNotes
+        },() => console.log(this.state.notes))
+    } 
+
     render() {
         return (
             <div className="NoteForm">
                 <form>
                 <p>
-                    <input type="text" name="title" placeholder="Title your note" value="" />
+                    <input 
+                        value={this.state.notes.title} 
+                        onChange={this.onTitleChange} 
+                        type="text" name="title" 
+                        placeholder="Title your note" 
+                    />
                 </p>
                 <p>
-                    <textarea name="body" cols="30" rows="10" placeholder="Just start typing..."></textarea>
+                    <textarea 
+                        onChange={this.onMessageChange}
+                        name="body" 
+                        cols="30" 
+                        rows="10" 
+                        placeholder="Just start typing...">
+                    </textarea>
                 </p>
                 </form>
             </div>
