1c7230eac0626497d095c3274c7393529ea272b9
Kyle Brown <brow1144@purdue.edu>
2017-06-14 18:54:46 -0400

    Create component for ListItem

---
+++ src/ListItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class ListItem extends Component {
+    render() {
+        return (
+            <li>
+                <div className="note">
+                    <div className="note-title">
+                        Citizens of distant epochs
+                    </div>
+                    <div className="note-body">
+                        <p>
+                            Sea of Tranquility the ash of stellar alchemy vastness is bearable only through love bits of moving fluff are creatures of the cosmos, consciousness a still more glorious dawn awaits two ghostly white figures in coveralls and helmets are soflty dancing tingling of the spine, concept of the number one brain is the seed of intelligence are creatures of the cosmos?
+                        </p>
+                    </div>
+                </div>
+            </li>
+        )
+    }
+}
+
+export default ListItem
