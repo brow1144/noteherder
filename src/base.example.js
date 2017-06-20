31b185d9d21994f05d496964a83678d71df2fba8
Kyle Brown <brow1144@purdue.edu>
2017-06-20 14:17:23 -0400

    Add bouncing effect on logout button and update base.example

--- src/base.example.js
+++ src/base.example.js
@@ -17,4 +17,6 @@ const db = database(app)
 export const auth = app.auth()
 export const githubProvider = new firebase.auth.GithubAuthProvider()
 
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+
 export default Rebase.createClass(db)
